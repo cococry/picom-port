edb24267a7d1405e4b22b6cde5466d5f56362144
Cococry <cococry.btw@gmail.com>
2024-07-16 21:17:09 +0200

    Files

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo cp ./picom /usr/local/bin
+sudo cp ./picom-trans /usr/local/bin
+sudo cp ./compton /usr/local/bin
+sudo cp ./compton-trans /usr/local/bin
+mkdir -p ~/.config/picom
+sudo cp ./picom.conf ~/.config/picom
